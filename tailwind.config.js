5201fdc045a4621cd7e861241152537e0501dab3
Jonathan Smithies <jonathan@elev8stud.io>
2021-06-09 15:06:42 +0100

    curvatureee

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   theme: {
     colors: {
       gold: '#c9963e',
-      'dark-gold': '#a9834a',
+      'dark-gold': '#b28843',
       purple: '#432673',
+      'purple-2': '#301c65',
+      'purple-3': '#3f2c78',
       black: '#111111',
       white: '#ffffff',
-      cream: '#ecdec8',
+      cream: '#e4d2b8',
       yellow: '#ffcf04',
       clear: 'transparent'
 
